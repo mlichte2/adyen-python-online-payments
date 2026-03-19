6612ca34a9b45fdaf11b1abf6b9aea59ec36e8fe
Michael Lichtenberger <michael.lichtenberger@adyen.com>
2026-03-19 09:41:32 -0500

    made changes to the advanced flow paymentMethods and payments payloads

--- app/static/js/advanced/_requestInfo.js
+++ app/static/js/advanced/_requestInfo.js
@@ -1,40 +1,57 @@
+/*
+
+Below are some constants 
+
+*/
+const shopperReference = "12345";
+
 // below is the data sent in the /paymentMethods call
 
 const paymentMethodsData = {
-    amount: {
-        value: 2495,
-        currency: "EUR"
-    },
-    countryCode: "NL"
-}
+  amount: {
+    value: 1500,
+    currency: "USD",
+  },
+  countryCode: "US",
+  shopperReference: shopperReference,
+};
 
-// the data sent in the /payments call
+// the data + plus paymentMethodsData object sent in the /payments call
 
 const paymentsData = {
-    ...paymentMethodsData,
-    reference: crypto.randomUUID(),
-    returnUrl: "http://" + window.location.host + "/handleShopperRedirect",
-    shopperEmail: "test@adyen.com",
-    shopperReference: "123",
-    redirectFromIssuerMethod: "POST",
-    lineItems: [
-        {
-            "quantity": "1",
-            "description": "Shoes",
-            "id": "Item #1",
-            "amountIncludingTax": "9000",
-            "productUrl": "URL_TO_PURCHASED_ITEM",
-            "imageUrl": "URL_TO_PICTURE_OF_PURCHASED_ITEM",
-            "itemCategory": "PHYSICAL_GOODS"
-        },
-        {
-            "quantity": "2",
-            "description": "Socks",
-            "id": "Item #2",
-            "amountIncludingTax": "1000",
-            "productUrl": "URL_TO_PURCHASED_ITEM",
-            "imageUrl": "URL_TO_PICTURE_OF_PURCHASED_ITEM",
-            "itemCategory": "PHYSICAL_GOODS"
-        }
-    ]
-}
+  ...paymentMethodsData,
+  reference: crypto.randomUUID(),
+  returnUrl: "http://" + window.location.host + "/handleShopperRedirect",
+  shopperEmail: "test@adyen.com",
+  shopperReference: shopperReference,
+  shopperInteraction: "Ecommerce",
+  recurringProcessingModel: "CardOnFile",
+  storePaymentMethod: false,
+  lineItems: [
+    {
+      quantity: "1",
+      description: "Shoes",
+      id: "Item #1",
+      amountIncludingTax: "9000",
+      productUrl: "URL_TO_PURCHASED_ITEM",
+      imageUrl: "URL_TO_PICTURE_OF_PURCHASED_ITEM",
+      itemCategory: "PHYSICAL_GOODS",
+    },
+    {
+      quantity: "2",
+      description: "Socks",
+      id: "Item #2",
+      amountIncludingTax: "1000",
+      productUrl: "URL_TO_PURCHASED_ITEM",
+      imageUrl: "URL_TO_PICTURE_OF_PURCHASED_ITEM",
+      itemCategory: "PHYSICAL_GOODS",
+    },
+  ],
+  authenticationData: {
+    threeDSRequestData: {
+      nativeThreeDS: "preferred",
+    },
+  },
+  channel: "Web",
+  origin: window.location.origin,
+};
